34cc78b5724b914338bb46cabf5f453ff41c2313
Brett <brett@mayson.io>
2022-07-11 00:08:13 -0600

    more names

--- addons/units/CfgWorlds.hpp
+++ addons/units/CfgWorlds.hpp
@@ -2,14 +2,37 @@ class CfgWorlds {
     class GenericNames {
         class PREFIX {
             class FirstNames {
+                andrew = "Andrew";
+                arsey = "Arsey";
+                avingon = "Avingon"
                 brett = "Brett";
-				thomas = "Thomas";
-				yurik = "Yurik";
+                carson = "Carson";
+                corey = "Corey";
+                jacob = "Jacob";
+                joffre = "Joffre";
+                jonathan = "Jonathan";
+                matias = "Matías";
+                nathanial = "Nathanial";
+                prince = "Prince";
+                richard = "Richard";
+                thomas = "Thomas";
+                yurik = "Yurik";
             };
             class LastNames {
-				harrison = "Harrison";
-				watson = "Watson";
-				stehlik = "Stehlik";
+                gerber = "Gerber";
+                greene = "Greene";
+                harrison = "Harrison";
+                johnson = "Johnson";
+                joseph = "Joseph";
+                libby = "Libby";
+                mcdowell = "McDowell";
+                miller = "Miller";
+                osburn = "Osburn";
+                pac = "Pac";
+                sering = "Sering";
+                singh = "Singh";
+                stehlik = "Stehlik";
+                watson = "Watson";
             };
         };
     };
